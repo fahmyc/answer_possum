f59d61a22bb993b9a2b9e9952789e4857fdc37c6
Chris Fahmy <chrisfahmy@hotmail.com>
2013-05-20 19:31:31 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AnswerPossum::Application.config.secret_token = '90971db2737586ddb6e689c436da7a4714f002cf07b292b549cb198eee6580cae4108859f4975470dea0a2f7550d7ccddabe2445b9c69bfd2e9df18486865c7c'
